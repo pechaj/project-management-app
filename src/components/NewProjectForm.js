cb770d4897de38fb8eca7b765ce60f73568d8552
pechaj <pechaj@gmail.com>
2025-09-02 09:21:59 +0200

    Project form fixes

--- src/components/NewProjectForm.js
+++ src/components/NewProjectForm.js
@@ -10,6 +10,8 @@ export default function NewProjectForm() {
             deleted : false
         });
 
+    const [sent, setSent] = useState(false);
+
     const handleChange = (event) => {
         setForm({
             ...form,
@@ -26,7 +28,7 @@ export default function NewProjectForm() {
             "code" : form?.code ?? "",
             "name" : form?.name ?? "",
             "notes" : form?.notes ?? "",
-            "deleted" : form?.deleted == "on" ? true : false
+            "deleted" : form?.deleted === "on" ? true : false
         };
 
         axios.post(`/api/Projects`, newProject)
@@ -36,33 +38,48 @@ export default function NewProjectForm() {
             .catch(error => {
                 console.log(error);
             });
+
+        setSent(true);
+
+        setForm({
+            code: "",
+            name: "",
+            notes: "",
+            deleted: false
+        });
+
     };
 
     return (
-        <div className='container mx-auto'>
-            <form className="m-5" onSubmit={handleSubmit} id="edit-project-form">
-                    <div className="form-group">
-                        <h5 className=''>Project editation form</h5>
-                    </div>
-                    <div className='m-3'>
-                        <label htmlFor="code" className='form-label'>Code</label>
-                        <input type="text" id="code" name="code" className='form-control' onChange={handleChange} />
-                    </div>
-                    <div className='m-3'>
-                        <label htmlFor="name" className='form-label'>Name</label>
-                        <input type="text" id="name" name="name" className='form-control' onChange={handleChange} />
-                    </div>
-                    <div className='m-3'>
-                        <label htmlFor="notes" className='form-label'>Notes</label>
-                        <textarea id="notes" name="notes" className='form-control' onChange={handleChange} />
-                    </div>
-                    <div className='m-3'>
-                        <label htmlFor="deleted" className='form-label'>Deleted</label>
-                        <input type="checkbox" id="deleted" name="deleted" className='form-check-input' onChange={handleChange} />
-                    </div>
+        <> {(sent) ? <Dialog open={sent} className='modal' onClose={() => setSent(false)}>
+            <div className='modal-title'>Project added successfully!</div>
+        </Dialog> : (
+            <div className='container mx-auto'>
+                <form className="m-5" onSubmit={handleSubmit} id="edit-project-form">
+                        <div className="form-group">
+                            <h5 className=''>Project editation form</h5>
+                        </div>
+                        <div className='m-3'>
+                            <label htmlFor="code" className='form-label'>Code</label>
+                            <input type="text" id="code" name="code" className='form-control' value={form.code} onChange={handleChange} />
+                        </div>
+                        <div className='m-3'>
+                            <label htmlFor="name" className='form-label'>Name</label>
+                            <input type="text" id="name" name="name" className='form-control' value={form.name} onChange={handleChange} />
+                        </div>
+                        <div className='m-3'>
+                            <label htmlFor="notes" className='form-label'>Notes</label>
+                            <textarea id="notes" name="notes" className='form-control' value={form.notes} onChange={handleChange} />
+                        </div>
+                        <div className='m-3'>
+                            <label htmlFor="deleted" className='form-label'>Deleted</label>
+                            <input type="checkbox" id="deleted" name="deleted" className='form-check-input' checked={form.deleted} onChange={handleChange} />
+                        </div>
 
-                    <button type="submit" className='btn btn-success'>Save</button>
-                </form>
-        </div>
+                        <button type="submit" className='btn btn-success'>Save</button>
+                    </form>
+            </div>
+        )}
+        </>
     )
 }
